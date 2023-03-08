2e02bdd035ad750fb1745a3abe94a689e87a9814
Soyoo.kim <soyoo0819@naver.com>
2023-03-09 00:05:08 +0900

    #7- 스프링부트 프로젝트 시작
    * 스프링웹
    *엑츄에이터
    *데브툴즈
    *롬복

---
+++ project-board-study/src/main/java/projectboardstudy/ProjectBoardStudyApplication.java
@@ -0,0 +1,13 @@
+package projectboardstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBoardStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBoardStudyApplication.class, args);
+    }
+
+}
---
+++ project-board-study/src/test/java/projectboardstudy/ProjectBoardStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package projectboardstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBoardStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
