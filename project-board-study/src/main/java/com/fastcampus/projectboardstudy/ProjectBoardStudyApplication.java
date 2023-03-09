6151c7913324224ce4a9974c843f204edc663cd4
Soyoo.kim <soyoo0819@naver.com>
2023-03-09 22:02:01 +0900

    #8- 도메인 클래스 설계(게시글, 댓글)
    도메인 정보를 설계했다,
    기초적인 게시글과 댓글 표현이 가능하도록함
    DB  와 연동을 위한 추가적인 표현은 추후에 업데이트 될 수 있음

--- project-board-study/src/main/java/com/fastcampus/projectboardstudy/ProjectBoardStudyApplication.java
+++ project-board-study/src/main/java/com/fastcampus/projectboardstudy/ProjectBoardStudyApplication.java
@@ -1,4 +1,4 @@
-package projectboardstudy;
+package com.fastcampus.projectboardstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- project-board-study/src/test/java/com/fastcampus/projectboardstudy/ProjectBoardStudyApplicationTests.java
+++ project-board-study/src/test/java/com/fastcampus/projectboardstudy/ProjectBoardStudyApplicationTests.java
@@ -1,4 +1,4 @@
-package projectboardstudy;
+package com.fastcampus.projectboardstudy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
